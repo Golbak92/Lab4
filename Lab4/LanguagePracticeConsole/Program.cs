b9be232557789b6eb6a09525f414e7650e83b1cf
Kristian Golba <kristian.golba@iths.se>
2020-10-08 15:56:44 +0200

    Added menu and folder creation in local app folder.

---
+++ Lab4/LanguagePracticeConsole/Program.cs
@@ -0,0 +1,49 @@
+﻿using LanguagePracticeProgram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguagePracticeConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Folder.CreateMap();
+
+            Console.WriteLine("Use any of the following parameters:");
+            Console.WriteLine("-lists");
+            Console.WriteLine("-new <listName><language 1><language 2>..<language n>");
+            Console.WriteLine("-add <listName>");
+            Console.WriteLine("-remove <listName><language><word 1><word 2>..<word n>");
+            Console.WriteLine("-words <listName><sortByLanguage>");
+            Console.WriteLine("-count <listName>");
+            Console.WriteLine("-practice <listName>");
+            string userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "lists":
+                    break;
+                case "new":
+                    break;
+                case "add":
+                    break;
+                case "remove":
+                    break;
+                case "words":
+                    break;
+                case "count":
+                    break;
+                case "practice":
+                    break;
+
+                default:
+                    Console.WriteLine();
+                    break;
+            }
+        }
+    }
+}
---
+++ Lab4/LanguagePracticeProgram/Folder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace LanguagePracticeProgram
+{
+    public class Folder
+    {
+        public static void CreateMap()
+        {
+            // The folder for the roaming current user 
+            string folder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+            // Combine the base folder with your specific folder....
+            string specificFolder = Path.Combine(folder, "test");
+
+            // CreateDirectory will check if folder exists and, if not, create it.
+            // If folder exists then CreateDirectory will do nothing.
+            Directory.CreateDirectory(specificFolder);
+        }    
+    }
+}
